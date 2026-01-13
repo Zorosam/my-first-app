4857f78c217b4ebd04b5c8c1e6a231e1b361761a
Aniiii <124913438+Zorosam@users.noreply.github.com>
2026-01-13 18:08:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 const list = document.getElementById("manga-list");
+const sources = JSON.parse(localStorage.getItem("sources")) || [];
 
-list.innerHTML = `
-  <p>Welcome 👋</p>
-  <p>Go to 📡 Manage Sources to add manga sources.</p>
-`;
+list.innerHTML = "";
+
+if (sources.length === 0) {
+  list.innerHTML = "<p>No sources added. Go to Manage Sources.</p>";
+}
+
+sources.forEach(url => {
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      data.manga.forEach(manga => {
+        const div = document.createElement("div");
+        div.style.padding = "10px";
+        div.style.margin = "10px 0";
+        div.style.border = "1px solid #ccc";
+        div.innerText = manga.title;
+
+        div.onclick = () => {
+          localStorage.setItem("selectedManga", JSON.stringify(manga));
+          window.location.href = "reader.html";
+        };
+
+        list.appendChild(div);
+      });
+    })
+    .catch(() => {
+      list.innerHTML += "<p>Failed to load a source.</p>";
+    });
+});
