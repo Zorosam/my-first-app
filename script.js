e10bb632efd3f1dcb09865a114e2ae13fc666c01
Aniiii <124913438+Zorosam@users.noreply.github.com>
2026-01-13 19:44:45 +0530

    Fetch manga data from sample-source.json
    Refactor manga list population to fetch data from a JSON file instead of local storage sources.

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
-const list = document.getElementById("manga-list");
-const sources = JSON.parse(localStorage.getItem("sources")) || [];
+fetch("sample-source.json")
+  .then(res => res.json())
+  .then(data => {
+    const list = document.getElementById("manga-list");
 
-list.innerHTML = "";
+    data.manga.forEach(manga => {
+      const div = document.createElement("div");
+      div.className = "manga-card";
+      div.innerText = manga.title;
 
-if (sources.length === 0) {
-  list.innerHTML = "<p>No sources added. Go to 📡 Manage Sources.</p>";
-}
+      div.onclick = () => {
+        localStorage.setItem("selectedManga", JSON.stringify(manga));
+        window.location.href = "reader.html";
+      };
 
-sources.forEach(url => {
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      data.manga.forEach(manga => {
-        const div = document.createElement("div");
-        div.className = "manga-card";
-        div.style.padding = "12px";
-        div.style.margin = "10px 0";
-        div.style.border = "1px solid black";
-        div.style.cursor = "pointer";
-
-        div.innerText = manga.title;
-
-        div.onclick = () => {
-          localStorage.setItem("selectedManga", JSON.stringify(manga));
-          window.location.href = "reader.html";
-        };
-
-        list.appendChild(div);
-      });
+      list.appendChild(div);
     });
-});
+  });
